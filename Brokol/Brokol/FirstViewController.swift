ec73aa76dc046e3fe80d2531f895622bb9fe7006
ammaarkhan <ammaarkhan03@gmail.com>
2022-11-26 01:14:49 -0800

    added first button

---
+++ Brokol/Brokol/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  Brokol
+//
+//  Created by Ammaar Khan on 25/11/2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "first screen"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
